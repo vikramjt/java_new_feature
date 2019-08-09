0b11fedf366eb0dc1bc74c52b8025a0d8969cf79
juivikram <juivikram@VikramPC>
2019-08-10 00:18:05 +0530

    Streams

--- JavaNewFeature/src/main/java/com/vikram/study/java/JavaNewFeature/StreamsDemo.java
+++ JavaNewFeature/src/main/java/com/vikram/study/java/JavaNewFeature/StreamsDemo.java
@@ -8,42 +8,53 @@ class StreamsDemo{
   { 
   
     // create a list of integers 
-    List<Integer> number = Arrays.asList(2,3,4,5); 
-  
+    List<Integer> numberList = Arrays.asList(4,6,5,10,9,5,9,10); 
+    System.out.println("Number List : "  + numberList); 
     // demonstration of map method 
-    List<Integer> square = number.stream().map(x -> x*x). 
+    List<Integer> square = numberList.stream().map(x -> x*x). 
                            collect(Collectors.toList()); 
-    System.out.println(square); 
+    System.out.println("Square of number List : "  + square + "\n"); 
   
     // create a list of String 
-    List<String> names = 
-                Arrays.asList("Reflection","Collection","Stream"); 
+    List<String> namesList = Arrays.asList("Reflection","Collection","Stream"); 
   
     // demonstration of filter method 
-    List<String> result = names.stream().filter(s->s.startsWith("S")). 
-                          collect(Collectors.toList()); 
-    System.out.println(result); 
+    System.out.println("List of String before filtering : " + namesList);
+    List<String> filterNameList = namesList.stream().filter(s->s.startsWith("S")).collect(Collectors.toList()); 
+    System.out.println("Result of using filter() method for string starting with S : " + filterNameList + "\n"); 
   
-    // demonstration of sorted method 
-    List<String> show = 
-            names.stream().sorted().collect(Collectors.toList()); 
-    System.out.println(show); 
+    //Demonstration of sorted method.
+    //It will be sorting based on the natural order.
+    System.out.println("List of String before sorting : " + namesList);
+    List<String> sortedStrList = namesList.stream().sorted().collect(Collectors.toList()); 
+    System.out.println("List of String after sorting : " + sortedStrList + "\n"); 
   
+    System.out.println("List of Integer before sorting : " + numberList);
+    List<Integer> sortedIntList = numberList.stream().sorted().collect(Collectors.toList()); 
+    System.out.println("List of String after sorting : " + sortedIntList + "\n"); 
+    
     // create a list of integers 
     List<Integer> numbers = Arrays.asList(2,3,4,5,2); 
   
+    System.out.println("Number of list to be put in set as Square : " + numbers);
     // collect method returns a set 
-    Set<Integer> squareSet = 
-         numbers.stream().map(x->x*x).collect(Collectors.toSet()); 
-    System.out.println(squareSet); 
+    Set<Integer> squareSet = numbers.stream().map(x->x*x).collect(Collectors.toSet()); 
+    System.out.println("Square number in Set collection of number List using collect: " + squareSet + "\n"); 
   
+    System.out.println("Number List given as in input to forEach() : " + numberList);
     // demonstration of forEach method 
-    number.stream().map(x->x*x).forEach(y->System.out.println(y)); 
-  
+    System.out.print("Print square of number List using forEach() : [" );
+    numberList.stream().map(x->x*x).forEach(y->System.out.print(y+ ", ")); 
+    System.out.print("]");
+    System.out.println("\n");
+    
     // demonstration of reduce method 
-    int even = 
-       number.stream().filter(x->x%2==0).reduce(0,(ans,i)-> ans+i); 
-  
-    System.out.println(even); 
+    System.out.println("Number list before giving to reduce() " + numberList);
+    int even = numberList.stream().filter(x->x%2==0).reduce(0,(ans,i)-> {
+    		System.out.println("ans : " + ans + " i : " + i);
+    		return (ans+i);
+    	}); 
+    System.out.println("Printing result number list using reduce() " + even); 
+    
   } 
 } 
